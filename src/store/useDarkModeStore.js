ba2c023a7418272428aa00c642bf32e04024cb43
Ananya Pati <126486682+ananyapati26@users.noreply.github.com>
2025-06-05 14:13:33 +0530

    Update useDarkModeStore.js

--- src/store/useDarkModeStore.js
+++ src/store/useDarkModeStore.js
@@ -1,7 +1,24 @@
 
+// import { create } from 'zustand';
+// const useDarkModeStore = create((set) => ({
+//   darkMode: false,
+//   setDarkMode: (value) => set({ darkMode: value }),
+// }));
+// export default useDarkModeStore;
+
 import { create } from 'zustand';
-const useDarkModeStore = create((set) => ({
-  darkMode: false,
-  setDarkMode: (value) => set({ darkMode: value }),
-}));
+import { persist } from 'zustand/middleware';
+
+const useDarkModeStore = create(
+  persist(
+    (set) => ({
+      darkMode: false,
+      setDarkMode: (value) => set({ darkMode: value }),
+    }),
+    {
+      name: 'dark-mode-storage', 
+    }
+  )
+);
+
 export default useDarkModeStore;
